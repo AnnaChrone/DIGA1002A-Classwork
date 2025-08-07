a5c69abb8d7eb441fc036a2629482582859cbc82
AnnaChrone <jnchronis1@gmail.com>
2025-08-07 13:11:41 +0200

    coding practice

---
+++ Classwork/Assets/Scripts/Coding.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
+using UnityEngine.UIElements;
+
+public class Coding : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+
+    }
+}
+
+public class Cat
+{
+
+}
